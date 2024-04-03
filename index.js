46e7327cf1f9aab84d43122c9f05662de1b98732
SheikhAnas6677 <162631863+SheikhAnas6677@users.noreply.github.com>
2024-04-03 11:54:46 +0500

    Add files via upload
    This calculator is made  by me

---
+++ index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstnumber" },
+    {
+        message: "Enter your second number",
+        type: "number",
+        name: "secondnumber",
+    },
+    {
+        message: "Select One of the operators to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please Select Valid Operator");
+}
